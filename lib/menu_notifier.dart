b0f36fc7f82e0e4ac0cf312a3b631a4f00c90002
meshachvijaya <meshachvijaya@gmail.com>
2023-12-14 17:01:34 +0700

    fix sub total

--- lib/menu_notifier.dart
+++ lib/menu_notifier.dart
@@ -10,6 +10,14 @@ class MenuNotifier extends ChangeNotifier {
     MenuItem(name: "Espresso", price: 17000),
   ];
 
+  int get totalAmount {
+    int total = 0;
+    for (var item in items) {
+      total += item.price * item.quantity;
+    }
+    return total;
+  }
+
   void incrementQuantity(int index) {
     if (items[index].quantity >= 0) {
       items[index].quantity++;
--- lib/screens/menu_list.dart
+++ lib/screens/menu_list.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../menu_notifier.dart';
-import '/menu_item.dart';
 
 class MenuList extends StatelessWidget {
   const MenuList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var menuNotifier = Provider.of<MenuNotifier>(context);
+    final menuNotifier = Provider.of<MenuNotifier>(context);
 
     return Scaffold(
       appBar: AppBar(
@@ -20,35 +19,53 @@ class MenuList extends StatelessWidget {
         backgroundColor: const Color(0xFF006041),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: menuNotifier.items.length,
-        itemBuilder: (context, index) {
-          MenuItem currentItem = menuNotifier.items[index];
-
-          return ListTile(
-            leading: const Icon(Icons.fastfood),
-            title: Text(currentItem.name),
-            subtitle: Text('Rp. ${currentItem.price}'),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: menuNotifier.items.length,
+              itemBuilder: (context, index) {
+                final currentItem = menuNotifier.items[index];
+                return ListTile(
+                  leading: const Icon(Icons.fastfood),
+                  title: Text(currentItem.name),
+                  subtitle: Text('Rp. ${currentItem.price}'),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.remove),
+                        onPressed: () {
+                          menuNotifier.decrementQuantity(index);
+                        },
+                      ),
+                      Text(currentItem.quantity.toString()),
+                      IconButton(
+                        icon: const Icon(Icons.add),
+                        onPressed: () {
+                          menuNotifier.incrementQuantity(index);
+                        },
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 8, 10, 50),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                IconButton(
-                  icon: const Icon(Icons.remove),
-                  onPressed: () {
-                    menuNotifier.decrementQuantity(index);
-                  },
-                ),
-                Text(currentItem.quantity.toString()),
-                IconButton(
-                  icon: const Icon(Icons.add),
-                  onPressed: () {
-                    menuNotifier.incrementQuantity(index);
-                  },
+                const Text("Total Items: "),
+                Text(
+                  'Rp. ${menuNotifier.totalAmount}',
+                  style: const TextStyle(fontWeight: FontWeight.w600),
                 ),
               ],
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
